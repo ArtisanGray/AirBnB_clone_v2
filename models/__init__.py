e3153dc854139003236dc6dc8e5aa75565b8d8a9
djso89 <danielso89@gmail.com>
2021-08-11 22:41:42 -0700

    typo on init in model

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,7 @@ if HBNB_TYPE_STORAGE =="db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    from models.enging.file_storage import FileStorage
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 
 
