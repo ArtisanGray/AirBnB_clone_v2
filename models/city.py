9e36b63c0dada40909af2d4dbdb186bcc2097a2f
djso89 <danielso89@gmail.com>
2021-08-12 12:07:37 -0700

    fixed the city, state, place model for task2 to work

--- models/city.py
+++ models/city.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = ""
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade="all, delete", backref='cities')
+    #places = relationship('Place', cascade='all, delete-orphan', backref='cities')
