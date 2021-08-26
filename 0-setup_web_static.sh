1f5b249d5a7b630c972efb87455d65bf7252fa41
ArtisanGray <1697@holbertonschool.com>
2021-08-26 23:57:28 +0000

    added 0, initial test commit

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#this script creates the necessary folders required for AirBnB
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo bash -c "mkdir -p /data/web_static/releases/test"
+sudo bash -c "mkdir -p /data/web_static/shared/"
+sudo bash -c "echo "this is a fake html file." > ~/data/web_static/releases/test/index.html"
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu ~/data/
+sudo sed -i '0,/server {/ !b;//a\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
