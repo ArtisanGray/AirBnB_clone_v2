29fe5fa22707f28d2ef12fce9a14bb9a759d7734
ArtisanGray <1697@holbertonschool.com>
2021-08-29 19:37:25 +0000

    added trailing slash on location append

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ sudo mkdir -p /data/web_static/shared/
 sudo bash -c "echo 'this is a fake html file.' > /data/web_static/releases/test/index.html"
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '0,/server {/c server {\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '0,/server {/c server {\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
