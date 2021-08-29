57fb2ad68d91c0fe7ac2276323347f70dce55119
ArtisanGray <1697@holbertonschool.com>
2021-08-29 18:58:12 +0000

    recreated 0, tested on web01

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,10 @@
 #this script creates the necessary folders required for AirBnB
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo bash -c "mkdir -p /data/web_static/releases/test"
-sudo bash -c "mkdir -p /data/web_static/shared/"
-sudo bash -c "echo "this is a fake html file." > ~/data/web_static/releases/test/index.html"
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+sudo bash -c "echo 'this is a fake html file.' > /data/web_static/releases/test/index.html"
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu ~/data/
-sudo sed -i '0,/server {/ !b;//a\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '0,/server {/c server {\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
